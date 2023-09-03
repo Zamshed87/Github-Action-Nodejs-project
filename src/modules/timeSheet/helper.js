b6cb89d3686a9f332d250097587774faa052bc4e
alaminsheikh01 <alaminsheikh9746@gmail.com>
2023-09-03 12:11:15 +0600

    updated on calender setup added wordplace

--- src/modules/timeSheet/helper.js
+++ src/modules/timeSheet/helper.js
@@ -16,6 +16,49 @@ export const createTimeSheetAction = async (payload, setLoading, cb) => {
     setLoading(false);
   }
 };
+
+export const createTimeSheetActionForCalender = async (payload, setLoading, cb) => {
+  try {
+    setLoading(true);
+    const res = await axios.post(
+      `/TimeSheet/TimeSheetCalender`,
+      payload
+    );
+    cb(res?.data?.autoId, res?.data?.autoName);
+    toast.success(res.data?.message || "Successful");
+    setLoading(false);
+  } catch (error) {
+    toast.warn(error?.response?.data?.message || "Something went wrong");
+    setLoading(false);
+  }
+};
+
+export const getPeopleDeskAllLandingForCalender = async (
+  busId,
+  setter,
+  setAllData,
+  setLoading,
+  pageNo,
+  pageSize,
+  search,
+) => {
+  setLoading && setLoading(true);
+
+  let Search = search ? `&searchTxt=${search}` : "";
+  try {
+    const res = await axios.get(
+      `/TimeSheet/GetCalenderLanding?IntBusinessUnitId=${busId}&PageNo=${pageNo}&PageSize=${pageSize}${Search}`
+    );
+    if (res?.data?.data) {
+      setter && setter(res?.data?.data);
+      setAllData && setAllData(res?.data?.data);
+      setLoading && setLoading(false);
+    }
+  } catch (error) {
+    setLoading && setLoading(false);
+  }
+};
+
 // search
 export const filterData = (keywords, gridData, setRowDto) => {
   const regex = new RegExp(keywords?.toLowerCase());
