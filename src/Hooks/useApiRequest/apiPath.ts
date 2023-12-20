fed221ddd493db8cb40a2c502f7b1e484510d032
tazbinahad <tazbinahad20@gmail.com>
2023-12-20 11:49:53 +0600

    Update Somthing

--- src/Hooks/useApiRequest/apiPath.ts
+++ src/Hooks/useApiRequest/apiPath.ts
@@ -20,7 +20,7 @@ export const apiPath = {
   GetOverTimeConfigById: "/Payroll/GetOverTimeConfigById",
   SectionDDL: "/SaasMasterData/SectionDDL",
   BonusAllLanding: "/Employee/BonusAllLanding",
-  CRUDBonusSetup: "https://localhost:7020/api/Employee/CRUDBonusSetup",
+  CRUDBonusSetup: "/Employee/CRUDBonusSetup",
   CheckBonusAlreadyExistInWorkPlace:
-    "https://localhost:7020/api/Employee/CheckBonusAlreadyExistInWorkPlace",
+    "/Employee/CheckBonusAlreadyExistInWorkPlace",
 };
