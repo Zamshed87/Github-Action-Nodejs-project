49f697b667a295fa0f47918027a735646cedbecf
utshaweu <utshaw1995@gmail.com>
2024-02-15 18:38:37 +0600

    separation history view print view added

--- src/modules/employeeProfile/separation/mgmApplication/viewForm/ApprovalList.jsx
+++ src/modules/employeeProfile/separation/mgmApplication/viewForm/ApprovalList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DataTable } from "Components";
-import PBadge from "Components/Badge";
-import FormikInput from "common/FormikInput";
+import { approvalListHeader } from "../utils";
 
 const ApprovalList = ({
   approveListData,
@@ -13,126 +12,19 @@ const ApprovalList = ({
   buttonType,
   setComment,
 }) => {
-  // Table Header
-  const header = [
-    {
-      title: "SL",
-      render: (value, row, index) => index + 1,
-      align: "center",
-      width: 20,
-    },
-    {
-      title: "Approve Dept.",
-      dataIndex: "strStatusTitle",
-      sorter: true,
-      render: (_, record) =>
-        record?.strStatusTitle === "Approve By Supervisor"
-          ? "Supervisor"
-          : record?.strStatusTitle === "Approve By User Group"
-          ? "User Group"
-          : "Line Manager",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-      align: "center",
-      render: (_, record) =>
-        // Write condition to check status
-        record?.status === "Pending" ? (
-          <PBadge type="warning" text={record?.status} />
-        ) : record?.status === "Rejected" ? (
-          <PBadge type="danger" text={record?.status} />
-        ) : (
-          <PBadge type="success" text={record?.status} />
-        ),
-      width: "50px",
-    },
-    {
-      title: "Comments",
-      dataIndex: "comment",
-      render: (_, record, index) =>
-        type === "approval" && record?.status === "Pending" ? (
-          <div>
-            <FormikInput
-              placeholder="Comments"
-              classes="input-sm"
-              name="comment"
-              type="text"
-              value={record?.comment}
-              onChange={(e) => {
-                rowDtoHandler(
-                  "comment",
-                  e.target.value,
-                  index,
-                  approveListData,
-                  setApproveListData
-                );
-                setComment(e.target.value);
-              }}
-              disabled={
-                data?.application?.intCurrentStage === record?.intPipelineRowId
-                  ? false
-                  : true
-              }
-            />
-          </div>
-        ) : (
-          record?.comment || "N/A"
-        ),
-    },
-    {
-      title: "Action",
-      dataIndex: "action",
-      align: "center",
-      render: (_, record) => {
-        return (
-          <div className="d-flex justify-content-center">
-            {record?.status === "Approved" || buttonType === "approve" ? (
-              <button
-                type="button"
-                onClick={() => demoPopup("approve", "Approve", data)}
-                className="btn btn-green"
-                disabled={
-                  data?.application?.intCurrentStage ===
-                  record?.intPipelineRowId
-                    ? false
-                    : true
-                }
-              >
-                Approve
-              </button>
-            ) : (
-              <button
-                type="button"
-                onClick={() => demoPopup("reject", "Reject", data)}
-                className="btn btn-cancel"
-                style={{
-                  backgroundColor: "#dc3545",
-                  color: "white",
-                  borderColor: "#dc3545",
-                }}
-                disabled={
-                  data?.application?.intCurrentStage ===
-                  record?.intPipelineRowId
-                    ? false
-                    : true
-                }
-              >
-                Reject
-              </button>
-            )}
-          </div>
-        );
-      },
-      isHidden: type === "view",
-    },
-  ].filter((item) => !item.isHidden);
-
   return (
     <>
       <div className="mt-3">
         <DataTable
-          header={header}
+          header={approvalListHeader({
+            type,
+            approveListData,
+            setApproveListData,
+            setComment,
+            data,
+            buttonType,
+            demoPopup,
+          })}
           bordered
           data={approveListData || []}
           loading={loading}
@@ -147,10 +39,3 @@ const ApprovalList = ({
 };
 
 export default ApprovalList;
-
-const rowDtoHandler = (name, value, sl, rowDto, setRowDto) => {
-  const data = [...rowDto];
-  const _sl = data[sl];
-  _sl[name] = value;
-  setRowDto(data);
-};
--- src/modules/employeeProfile/separation/mgmApplication/viewForm/AssetHistory.jsx
+++ src/modules/employeeProfile/separation/mgmApplication/viewForm/AssetHistory.jsx
@@ -1,55 +1,12 @@
 import React from "react";
 import { DataTable } from "Components";
-import { Tag } from "antd";
-import moment from "moment";
+import { assetHeader } from "../utils";
 
 const AssetHistory = ({ assetHistory, loading }) => {
-  const header = [
-    {
-      title: "SL",
-      render: (value, row, index) => index + 1,
-      align: "center",
-      width: 20,
-    },
-    {
-      title: "Item Name",
-      dataIndex: "itemName",
-      sorter: true,
-    },
-    {
-      title: "Item Qty",
-      dataIndex: "itemQuantity",
-    },
-    {
-      title: "UOM",
-      dataIndex: "itemUom",
-    },
-    {
-      title: "Assign Date",
-      dataIndex: "assignDate",
-      render: (data, record) => moment(record?.assignDate).format("DD-MM-YYYY"),
-    },
-    {
-      title: "Status",
-      dataIndex: "active",
-      align: "center",
-      render: (_, rec) => {
-        return (
-          <div className="d-flex justify-content-center align-items-center">
-            {rec?.active ? (
-              <Tag color="green">{"Active"}</Tag>
-            ) : (
-              <Tag color="red">{"Inactive"}</Tag>
-            )}
-          </div>
-        );
-      },
-    },
-  ];
   return (
     <div className="mt-3">
       <DataTable
-        header={header}
+        header={assetHeader}
         bordered
         data={assetHistory || []}
         loading={loading}
