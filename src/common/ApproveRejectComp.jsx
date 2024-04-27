5b4faf0ceb0bc8ee0c2490108f7f7cfa81fa8822
iboscopilot <eaccount@ibos.io>
2024-04-27 12:12:22 +0600

    approval button component design added

--- src/common/ApproveRejectComp.jsx
+++ src/common/ApproveRejectComp.jsx
@@ -1,3 +1,7 @@
+import { Cancel, CheckCircle } from "@mui/icons-material";
+import MuiIcon from "./MuiIcon";
+import { failColor, successColor } from "utility/customColor";
+
 /* eslint-disable @typescript-eslint/no-empty-function */
 const ApproveRejectComp = ({ props = {} }) => {
   const { className, onApprove = () => {}, onReject = () => {} } = props || {};
@@ -5,7 +9,7 @@ const ApproveRejectComp = ({ props = {} }) => {
     <div className={className}>
       <div
         style={{
-          width: "140px",
+          width: "180px",
         }}
         className="custom-approve-reject-flex-btn"
       >
@@ -17,12 +21,42 @@ const ApproveRejectComp = ({ props = {} }) => {
             gap: "8px",
           }}
         >
-          <button type="button" className="btn-approve" onClick={onApprove}>
-            Approve
-          </button>
-          <button type="button" className="btn-reject" onClick={onReject}>
-            Reject
-          </button>
+          <div onClick={onApprove} className="d-flex justify-content-start align-items-center approval-btn-container-approve">
+            <MuiIcon
+              icon={
+                <CheckCircle
+                className="icon-approve"
+                  sx={{
+                    color: successColor,
+                    width: "20px !important",
+                    height: "33px !important",
+                    fontSize: "20px !important",
+                  }}
+                />
+              }
+            />
+            <button type="button" className="btn-approve">
+              <span>Approve</span>
+            </button>
+          </div>
+          <div onClick={onReject} className="d-flex justify-content-start align-items-center approval-btn-container-reject">
+            <MuiIcon
+              icon={
+                <Cancel
+                className="icon-reject"
+                  sx={{
+                    color: failColor,
+                    width: "20px !important",
+                    height: "33px !important",
+                    fontSize: "20px !important",
+                  }}
+                />
+              }
+            />
+            <button type="button" className="btn-reject">
+              <span>Reject</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
