87c0a4d71d585913425fd306dcc6ee9f7f13bda5
iboscopilot <eaccount@ibos.io>
2024-07-09 14:51:24 +0600

    update: holiday off day swaping new landing added

--- src/modules/TimeManagement/HolidayOffdaySwap/index.tsx
+++ src/modules/TimeManagement/HolidayOffdaySwap/index.tsx
@@ -1,27 +1,17 @@
-import {
-  DataTable,
-  PButton,
-  PCard,
-  PCardBody,
-  PCardHeader,
-  PForm,
-  PInput,
-  PSelect,
-} from "Components";
-import { useApiRequest } from "Hooks";
-import { Col, Form, Row } from "antd";
+import { Form } from "antd";
 import NotPermittedPage from "common/notPermitted/NotPermittedPage";
+import { setFirstLevelNameAction } from "commonRedux/reduxForLocalStorage/actions";
+import { DataTable, PCard, PCardHeader, PForm } from "Components";
+import { useApiRequest } from "Hooks";
 import { useEffect, useMemo, useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { attendenceStatusDDL, header } from "./helper";
-import { setFirstLevelNameAction } from "commonRedux/reduxForLocalStorage/actions";
-import { PModal } from "Components/Modal";
-import { toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
+import { holidayOffdaySwapLandingheader } from "./helper";
 
 const HolidayOffdaySwap = () => {
   const {
     permissionList,
-    profileData: { wId, wgId, buId, orgId, employeeId },
+    profileData: { wId, wgId, buId },
   } = useSelector((state: any) => state?.auth, shallowEqual);
   const dispatch = useDispatch();
 
@@ -35,147 +25,44 @@ const HolidayOffdaySwap = () => {
 
   // states
   const [rowDto, setRowDto] = useState<any[]>([]);
-  const [selectedRow, setSelectedRow] = useState<any[]>([]);
-  const [open, setOpen] = useState<boolean>(false);
   // api states
-  const empDepartmentDDL = useApiRequest([]);
-  const empDesignationDDL = useApiRequest([]);
-  const positionDDL = useApiRequest([]);
   const holidayOffdayLandingAPI = useApiRequest([]);
-  const holidayOffdaySwapPostAPI = useApiRequest([]);
+  const history = useHistory();
 
-  const getEmployeDepartment = () => {
-    empDepartmentDDL?.action({
-      urlKey: "PeopleDeskAllDDL",
-      method: "GET",
-      params: {
-        DDLType: "EmpDepartment",
-        BusinessUnitId: buId,
-        WorkplaceGroupId: wgId,
-        IntWorkplaceId: wId,
-        intId: 0,
-      },
-      onSuccess: (res) => {
-        res.forEach((item: any, i: any) => {
-          res[i].label = item?.DepartmentName;
-          res[i].value = item?.DepartmentId;
-        });
-      },
-    });
-  };
-  const getEmployeDesignation = () => {
-    empDesignationDDL?.action({
-      urlKey: "PeopleDeskAllDDL",
-      method: "GET",
-      params: {
-        DDLType: "EmpDesignation",
-        AccountId: orgId,
-        BusinessUnitId: buId,
-        WorkplaceGroupId: wgId,
-        IntWorkplaceId: wId,
-        intId: 0,
-      },
-      onSuccess: (res) => {
-        res.forEach((item: any, i: any) => {
-          res[i].label = item?.DesignationName;
-          res[i].value = item?.DesignationId;
-        });
-      },
-    });
-  };
-  const getEmployeePosition = () => {
-    positionDDL?.action({
-      urlKey: "PeopleDeskAllDDL",
-      method: "GET",
-      params: {
-        DDLType: "Position",
-        BusinessUnitId: buId,
-        WorkplaceGroupId: wgId,
-        IntWorkplaceId: wId,
-        intId: 0,
-      },
-      onSuccess: (res) => {
-        res.forEach((item: any, i: any) => {
-          res[i].label = item?.PositionName;
-          res[i].value = item?.PositionId;
-        });
-      },
-    });
-  };
-  const postholidayOffdaySwap = () => {
-    const { attendenceStatus, attendenceDate, changedTo } =
-      form.getFieldsValue(true);
-    holidayOffdaySwapPostAPI?.action({
-      urlKey: "holidayOffdayAssignPost",
-      method: "post",
-      payload: {
-        attendenceStatus: attendenceStatus?.value,
-        attendenceDate: attendenceDate,
-        empIdList: selectedRow?.map((dto) => dto?.intEmpId).toString(),
-        swapDate: changedTo,
-        empId: employeeId,
-      },
-      onSuccess: () => {
-        setOpen(false);
-        setSelectedRow([]);
-        getholidayOffdayLanding();
-      },
-    });
-  };
   const getholidayOffdayLanding = (
     pagination = { current: 1, pageSize: 25 }
   ) => {
-    const {
-      departments,
-      designatios,
-      hrPositions,
-      search,
-      attendenceStatus,
-      attendenceDate,
-    } = form.getFieldsValue(true);
-
+    const { search } = form.getFieldsValue(true);
+    // api: /Employee/SwapEmployeeHistory?workplaceGroupId=4&workplaceId=15&CurrentPage=1&PageSize=15
     holidayOffdayLandingAPI?.action({
-      urlKey: "holidayOffdayLanding",
-      method: "post",
-      payload: {
-        workplace: wId,
-        workplaceGroup: wgId,
-        hrPositions: hrPositions?.map((dto: any) => dto?.value) || [],
-        departments: departments?.map((dto: any) => dto?.value) || [],
-        designatios: designatios?.map((dto: any) => dto?.value) || [],
+      urlKey: "SwapEmployeeHistory",
+      method: "get",
+      params: {
+        workplaceId: wId,
+        workplaceGroupId: wgId,
         searchTxt: search || "",
-        attendenceStatus: attendenceStatus?.value,
-        attendenceDate: attendenceDate,
-        pageNo: pagination?.current,
-        pageSize: pagination?.pageSize,
+        CurrentPage: pagination?.current,
+        PageSize: pagination?.pageSize,
       },
       onSuccess: (res) => {
-        setRowDto(res);
+        setRowDto(res?.swapEmployeeHistoryDTO);
       },
     });
   };
 
   // Life Cycle Hooks
   useEffect(() => {
-    getholidayOffdayLanding();
     dispatch(setFirstLevelNameAction("Administration"));
     document.title = "Holiday/Offday Swap";
   }, []);
+
   useEffect(() => {
-    getEmployeDepartment();
-    getEmployeDesignation();
-    getEmployeePosition();
+    getholidayOffdayLanding();
   }, [wgId, buId, wId]);
 
   return holidayOffdayPermission?.isView ? (
     <>
-      <PForm
-        form={form}
-        initialValues={{}}
-        onFinish={() => {
-          getholidayOffdayLanding();
-        }}
-      >
+      <PForm form={form} initialValues={{}}>
         <PCard>
           <PCardHeader
             title="Holiday/Offday Swap"
@@ -188,182 +75,42 @@ const HolidayOffdaySwap = () => {
             buttonList={[
               {
                 type: "primary",
-                content: `Swap ${selectedRow?.length || ""}`,
+                content: `Assign Swap`,
                 onClick: () => {
-                  setOpen(true);
+                  history.push(
+                    "/administration/timeManagement/holidayOffdaySwap/assign"
+                  );
                 },
-                disabled: selectedRow?.length === 0 ? true : false,
+                disabled: !holidayOffdayPermission?.isCreate,
               },
             ]}
           />
-          <PCardBody className="mb-3">
-            <Row gutter={[10, 2]}>
-              <Col md={6} sm={12} xs={24}>
-                <PSelect
-                  options={empDepartmentDDL?.data || []}
-                  name="departments"
-                  showSearch
-                  filterOption={true}
-                  label="Department"
-                  allowClear
-                  mode="multiple"
-                  placeholder="Department"
-                  onChange={(value, op) => {
-                    form.setFieldsValue({
-                      departments: op,
-                    });
-                  }}
-                />
-              </Col>
-              <Col md={6} sm={12} xs={24}>
-                <PSelect
-                  options={empDesignationDDL.data || []}
-                  showSearch
-                  allowClear
-                  filterOption={true}
-                  name="designatios"
-                  label="Designation"
-                  mode="multiple"
-                  placeholder="Designation"
-                  onChange={(value, op) => {
-                    form.setFieldsValue({
-                      designatios: op,
-                    });
-                  }}
-                />
-              </Col>
-              <Col md={6} sm={12} xs={24}>
-                <PSelect
-                  options={positionDDL?.data || []}
-                  name="hrPositions"
-                  mode="multiple"
-                  showSearch
-                  allowClear
-                  filterOption={true}
-                  label="HR Position"
-                  placeholder="HR Position"
-                  onChange={(value, op) => {
-                    form.setFieldsValue({
-                      hrPositions: op,
-                    });
-                  }}
-                />
-              </Col>
 
-              <Col md={6} sm={12} xs={24}>
-                <PSelect
-                  options={attendenceStatusDDL}
-                  name="attendenceStatus"
-                  showSearch
-                  filterOption={true}
-                  label="Holiday/Offday"
-                  placeholder="Holiday/Offday"
-                  onChange={(value, op) => {
-                    form.setFieldsValue({
-                      attendenceStatus: op,
-                    });
-                  }}
-                  rules={[
-                    {
-                      required: true,
-                      message: "Attendence Status Is Required",
-                    },
-                  ]}
-                />
-              </Col>
-              <Col md={6} sm={12} xs={24}>
-                <PInput
-                  type="date"
-                  name="attendenceDate"
-                  placeholder="Attendence Date"
-                  label="Attendence Date"
-                  rules={[
-                    { required: true, message: "Attendence Date Is Required" },
-                  ]}
-                />
-              </Col>
-              <Col md={6} sm={12} xs={24}>
-                <PButton
-                  action="submit"
-                  style={{ marginTop: "22px" }}
-                  type="primary"
-                  content="View"
-                />
-              </Col>
-            </Row>
-          </PCardBody>
           <DataTable
-            header={header(holidayOffdayLandingAPI)}
+            header={holidayOffdaySwapLandingheader()}
             bordered
             data={rowDto || []}
             loading={holidayOffdayLandingAPI?.loading}
             pagination={{
-              current: holidayOffdayLandingAPI?.data[0]?.currentPage,
-              pageSize: holidayOffdayLandingAPI?.data[0]?.pageSize,
-              total: holidayOffdayLandingAPI?.data[0]?.totalCount,
+              current: holidayOffdayLandingAPI?.data?.currentPage,
+              pageSize: holidayOffdayLandingAPI?.data?.pageSize,
+              total: holidayOffdayLandingAPI?.data?.totalCount,
             }}
-            rowSelection={{
-              type: "checkbox",
-              selectedRowKeys: selectedRow.map((item) => item?.key),
-              onChange: (selectedRowKeys, selectedRows) => {
-                setSelectedRow(selectedRows);
-              },
-            }}
+            // rowSelection={{
+            //   type: "checkbox",
+            //   selectedRowKeys: selectedRow.map((item) => item?.key),
+            //   onChange: (selectedRowKeys, selectedRows) => {
+            //     setSelectedRow(selectedRows);
+            //   },
+            // }}
             onChange={(pagination, filters, sorter, extra) => {
               if (extra.action === "sort") return;
               getholidayOffdayLanding(pagination);
-              setSelectedRow([]);
+              // setSelectedRow([]);
             }}
           />
         </PCard>
       </PForm>
-      <PModal
-        open={open}
-        title={`Swap Holiday/Offday`}
-        onCancel={() => {
-          setOpen(false);
-          form.setFieldValue("changedTo", "");
-        }}
-        width={800}
-        components={
-          <Row gutter={[10, 2]}>
-            <Col md={6} sm={12} xs={24}>
-              <PInput
-                type="date"
-                name="changedTo"
-                placeholder="Changed to"
-                label="Changed to"
-                onChange={(value) => {
-                  form.setFieldValue("changedTo", value);
-                }}
-              />
-            </Col>
-            <Col md={6} sm={12} xs={24}>
-              <PButton
-                action="button"
-                style={{ marginTop: "34px" }}
-                type="primary"
-                content="Save"
-                onClick={() => {
-                  if (form.getFieldValue("changedTo")) {
-                    if (
-                      form.getFieldValue("changedTo").format("YYYY-MM-DD") ===
-                      form.getFieldValue("attendenceDate").format("YYYY-MM-DD")
-                    ) {
-                      return toast.error(
-                        "Attendance date and Swaped date cannot be same"
-                      );
-                    }
-                    postholidayOffdaySwap();
-                  } else {
-                    return toast.warning("please give a valid date");
-                  }
-                }}
-              />
-            </Col>
-          </Row>
-        }
-      />
     </>
   ) : (
     <NotPermittedPage />
