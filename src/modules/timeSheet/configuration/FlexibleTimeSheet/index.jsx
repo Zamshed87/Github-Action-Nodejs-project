f532b8112ea07f48cd56453e9088ed6ca0160435
Khurshida Meem <meemrumi59@gmail.com>
2024-09-15 11:17:46 +0600

    updated empdashboard

--- src/modules/timeSheet/configuration/FlexibleTimeSheet/index.jsx
+++ src/modules/timeSheet/configuration/FlexibleTimeSheet/index.jsx
@@ -1,11 +1,17 @@
+import { Form } from "antd";
 import { setFirstLevelNameAction } from "commonRedux/reduxForLocalStorage/actions";
-import { PForm } from "Components";
+import { PCardHeader, PForm, PSelect } from "Components";
+import { useApiRequest } from "Hooks";
+import { debounce } from "lodash";
 import moment from "moment";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 
 const FlexibleTimeSheet = () => {
   const dispatch = useDispatch();
+
+  const { isSuperuser, isOfficeAdmin, buId, wgId, wId, employeeId, orgId } =
+    useSelector((state) => state?.auth?.profileData, shallowEqual);
 
   useEffect(() => {
     dispatch(setFirstLevelNameAction("Administration"));
@@ -13,6 +19,60 @@ const FlexibleTimeSheet = () => {
     () => {
       document.title = "PeopleDesk";
     };
+  }, []);
+
+  // Form Instance
+  const [form] = Form.useForm();
+
+  // api states
+  const empDepartmentDDL = useApiRequest([]);
+  const supervisorDDL = useApiRequest([]);
+
+  const getEmployeDepartment = () => {
+    empDepartmentDDL?.action({
+      urlKey: "PeopleDeskAllDDL",
+      method: "GET",
+      params: {
+        DDLType: "EmpDepartment",
+        BusinessUnitId: buId,
+        WorkplaceGroupId: wgId,
+        IntWorkplaceId: wId,
+        intId: 0,
+      },
+      onSuccess: (res) => {
+        res.forEach((item, i) => {
+          res[i].label = item?.DepartmentName;
+          res[i].value = item?.DepartmentId;
+        });
+      },
+    });
+  };
+
+  const getSuperVisorDDL = debounce((value) => {
+    if (value?.length < 2) return supervisorDDL?.reset();
+    supervisorDDL?.action({
+      urlKey: "PeopleDeskAllDDL",
+      method: "GET",
+      params: {
+        DDLType: "EmployeeBasicInfoForEmpMgmt",
+        AccountId: orgId,
+        BusinessUnitId: buId,
+        intId: employeeId,
+        workplaceGroupId: wgId,
+        strWorkplaceIdList: wId,
+        searchTxt: value || "",
+      },
+      onSuccess: (res) => {
+        res.forEach((item, i) => {
+          res[i].label = item?.EmployeeOnlyName;
+          res[i].value = item?.EmployeeId;
+        });
+      },
+    });
+  }, 500);
+
+  useEffect(() => {
+    isOfficeAdmin && getEmployeDepartment();
   }, []);
 
   return (
@@ -23,7 +83,26 @@ const FlexibleTimeSheet = () => {
         fDate: moment(),
         tDate: moment(),
       }}
-    ></PForm>
+    >
+      <PCardHeader title="Flexible Timesheet">
+        {isOfficeAdmin && (
+          <PSelect
+            options={empDepartmentDDL?.data || []}
+            name="department"
+            placeholder="Select Department"
+            style={{ width: "200px" }}
+            onSelect={(value, op) => {}}
+          />
+        )}
+        <PSelect
+          options={empDepartmentDDL?.data || []}
+          name="supervisor"
+          placeholder="Change Attendance"
+          style={{ width: "200px" }}
+          onSelect={(value, op) => {}}
+        />
+      </PCardHeader>
+    </PForm>
   );
 };
 
