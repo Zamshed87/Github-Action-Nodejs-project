46b1a6d57406d2ab25106b4c2d89e5fd7af61cbc
mdsifat <ibnsifat@gmail.com>
2024-09-19 10:15:28 +0600

    update leave policy

--- src/modules/leaveMovement/configuration/YearlyLeavePolicy/CreateEditLeavePolicy.tsx
+++ src/modules/leaveMovement/configuration/YearlyLeavePolicy/CreateEditLeavePolicy.tsx
@@ -207,10 +207,15 @@ const CreateEditLeavePolicy = () => {
           history.push("/administration/leaveandmovement/yearlyLeavePolicy");
         }
       },
-      onError: (error) => {
-        toast.error(error?.message || "Something went wrong", {
-          toastId: "savePolicyError",
-        });
+      onError: (error: any) => {
+        toast.error(
+          error?.response?.data?.message ||
+            error?.message ||
+            "Something went wrong",
+          {
+            toastId: "savePolicyError",
+          }
+        );
       },
     });
   };
