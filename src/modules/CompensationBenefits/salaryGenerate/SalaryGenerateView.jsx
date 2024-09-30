e6c99ed57a0f048b9e4ec5d73894db35831a14b0
Khurshida Meem <meemrumi59@gmail.com>
2024-09-30 14:06:52 +0600

    updated

--- src/modules/CompensationBenefits/salaryGenerate/SalaryGenerateView.jsx
+++ src/modules/CompensationBenefits/salaryGenerate/SalaryGenerateView.jsx
@@ -310,7 +310,7 @@ const SalaryGenerateView = () => {
           text
         ),
       onCell: (record) => ({
-        colSpan: record?.DeptName?.trim() ? 9 : 1,
+        colSpan: record?.DeptName?.trim() ? 10 : 1,
       }),
       width: 40,
       fixed: "left",
