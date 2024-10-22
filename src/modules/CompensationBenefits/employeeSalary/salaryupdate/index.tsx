1c84c54f75d326f2f80701e098ebedc18866b9dd
mdsifat <ibnsifat@gmail.com>
2024-10-22 15:32:49 +0600

    update

--- src/modules/CompensationBenefits/employeeSalary/salaryupdate/index.tsx
+++ src/modules/CompensationBenefits/employeeSalary/salaryupdate/index.tsx
@@ -7,6 +7,8 @@ import {
   PInput,
   PSelect,
 } from "Components";
+import profileImg from "../../../../assets/images/profile.jpg";
+
 import { useApiRequest } from "Hooks";
 import { Col, Form, Row } from "antd";
 import NoResult from "common/NoResult";
@@ -15,6 +17,9 @@ import { setFirstLevelNameAction } from "commonRedux/reduxForLocalStorage/action
 import moment from "moment";
 import React, { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { gray200, gray700, gray900 } from "utility/customColor";
+import { APIUrl } from "App";
+import { MovingOutlined } from "@mui/icons-material";
 
 type TAttendenceAdjust = unknown;
 const SalaryV2: React.FC<TAttendenceAdjust> = () => {
@@ -703,6 +708,153 @@ const SalaryV2: React.FC<TAttendenceAdjust> = () => {
             },
           ]}
         ></PCardHeader>
+        <Row gutter={[10, 2]} className="mb-3 card-style">
+          <Col md={13}>
+            <div
+              className="d-flex justify-content-between align-items-center mt-2"
+              style={{
+                paddingBottom: "10px",
+                marginBottom: "10px",
+                // borderBottom: `1px solid ${gray200}`,
+              }}
+            >
+              <div className="d-flex ">
+                <div
+                  style={{
+                    // width: singleData > 0 ? singleData && "auto" : "78px",
+                    width: [].length > 0 ? "auto" : "78px",
+                  }}
+                  className={
+                    // singleData > 0
+                    //   ? singleData && "add-image-about-info-card height-auto"
+                    //   : "add-image-about-info-card"
+                    [].length > 0
+                      ? "add-image-about-info-card height-auto"
+                      : "add-image-about-info-card"
+                  }
+                >
+                  <label
+                    htmlFor="contained-button-file"
+                    className="label-add-image"
+                  >
+                    {false ? ( //singleData[0]?.ProfileImageUrl
+                      <img
+                        src={`${APIUrl}/Document/DownloadFile?id=${0}`}
+                        alt=""
+                        height="78px"
+                        width="78px"
+                        style={{ maxHeight: "78px", minWidth: "78px" }}
+                      />
+                    ) : (
+                      <img
+                        src={profileImg}
+                        alt="iBOS"
+                        height="78px"
+                        width="78px"
+                        style={{ maxHeight: "78px", minWidth: "78px" }}
+                      />
+                    )}
+                  </label>
+                </div>
+                <div className="content-about-info-card ml-3">
+                  <div className="d-flex justify-content-between">
+                    <h4
+                      className="name-about-info"
+                      style={{ marginBottom: "5px" }}
+                    >
+                      {/* {`${singleData[0]?.EmployeeName}  `}  */}
+                      <span style={{ fontWeight: "400", color: gray700 }}>
+                        {/* [{singleData[0]?.EmployeeCode}] */}
+                      </span>{" "}
+                    </h4>
+                  </div>
+                  <div className="single-info">
+                    <p
+                      className="text-single-info"
+                      style={{ fontWeight: "500", color: gray700 }}
+                    >
+                      <small style={{ fontSize: "12px", lineHeight: "1.5" }}>
+                        Department -
+                      </small>{" "}
+                      {/* {`${singleData[0]?.DepartmentName}`} */}
+                    </p>
+                  </div>
+                  <div className="single-info">
+                    <p
+                      className="text-single-info"
+                      style={{ fontWeight: "500", color: gray700 }}
+                    >
+                      <small style={{ fontSize: "12px", lineHeight: "1.5" }}>
+                        Designation -
+                      </small>{" "}
+                      {/* {singleData[0]?.DesignationName} */}
+                    </p>
+                  </div>
+                  <div className="single-info">
+                    <p
+                      className="text-single-info"
+                      style={{ fontWeight: "500", color: gray700 }}
+                    >
+                      <small style={{ fontSize: "12px", lineHeight: "1.5" }}>
+                        Employment Type -
+                      </small>{" "}
+                      {/* {singleData[0]?.strEmploymentType} */}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Col>
+          <Col md={8}></Col>
+          <div className="">
+            {/* <FormikCheckBox
+                height="15px"
+                styleObj={{
+                  color: gray900,
+                  checkedColor: greenColor,
+                  padding: "0px 0px 0px 5px",
+                }}
+                label={"Hold Salary"}
+                name="isHoldSalary"
+                value={isHoldSalary}
+                checked={isHoldSalary}
+                onChange={(e) => {
+                  // setIsHoldSalary(e.target.checked);
+                  // holdSalaryHandler(e);
+                }}
+              /> */}
+            <div className="ml-1">
+              <PInput
+                label="Hold Salary?"
+                type="checkbox"
+                layout="horizontal"
+                name="isHoldSalary"
+              />
+            </div>
+            <div>
+              <p
+                onClick={(e) => {
+                  e.stopPropagation();
+                  // setOpenIncrement(true);
+                  // setIsOpen(false);
+                }}
+                style={{ color: gray900 }}
+                className="d-inline-block mt-2 pointer uplaod-para"
+              >
+                <span style={{ fontSize: "12px" }}>
+                  <MovingOutlined
+                    sx={{
+                      marginRight: "5px",
+                      fontSize: "18px",
+                      color: gray900,
+                    }}
+                  />{" "}
+                  Increment History
+                </span>
+              </p>
+            </div>
+          </div>
+        </Row>
         <Row gutter={[10, 2]} className="mb-3">
           {/* <Col md={6} sm={12} xs={24}>
             <PSelect
