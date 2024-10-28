e3c1c74e6e0ab68f68aa152c12bff2c5f8be16fe
alaminsheikh01 <alaminsheikh9746@gmail.com>
2024-10-28 13:36:21 +0600

    update: added a new overtime amount on range based round

--- src/modules/PayrollManagementSytem/OvertimePolicy/Utils.ts
+++ src/modules/PayrollManagementSytem/OvertimePolicy/Utils.ts
@@ -43,16 +43,39 @@ export const OTCountFrom = [
     label: "Round Up",
   },
 ];
+export const OTCountAmount = [
+  { value: 1, label: "At Actual" },
+  {
+    value: 2,
+    label: "Round Down",
+  },
+  {
+    value: 3,
+    label: "Round Up",
+  },
+  {
+    value: 4,
+    label: "Range Based Round",
+  },
+];
+export const BasedOn = [
+  { value: 1, label: "Fixed" },
+
+  { value: 2, label: "Actual" },
+];
 // Payload Generation for SaveNUpdateOverTimeConfig => Line 16 - 111
 type TOTPolicyGenerate = {
   values: any;
   commonData: any;
   matchingData: any;
   state: any;
+  tableData: any;
 };
+
 export const OTPolicyGenerate = ({
   commonData,
   values,
+  tableData,
 }: // matchingData,
 TOTPolicyGenerate) => {
   const {
@@ -80,6 +103,16 @@ TOTPolicyGenerate) => {
     .map(({ value, label }) => ({ value, label }));
   // console.log({ policy });
 
+  const serviceLengthList =
+    tableData?.map((item: any, idx: number) => {
+      return {
+        intFromMinute: item?.intFromMinute || 0,
+        intToMinute: item?.intToMinute || 0,
+        isAtActual: item?.showInDepend?.value === 2 ? true : false,
+        intOvertimeMinute: item?.intOvertimeMinute || 0,
+      };
+    }) || [];
+
   const policyInfo = {
     intOtconfigId: intOtconfigId || 0,
     intMaxOverTimeDaily: values?.maxOverTimeDaily || 0,
@@ -104,12 +137,18 @@ TOTPolicyGenerate) => {
     // isOffdayCountAsFullDayOt: values?.count === 2 ? true : false,
     isHolidayCountAsFullDayOt: values?.isHolidayCountAsFullDayOt ? true : false,
     isOffdayCountAsFullDayOt: values?.isOffdayCountAsFullDayOt ? true : false,
-    intMaxOverTimeForHolidayInMin: values?.intMaxOverTimeForHolidayInMin * 60 || 0,
-    intMaxOverTimeForOffdayInMin: values?.intMaxOverTimeForOffdayInMin * 60 || 0,
+    intMaxOverTimeForHolidayInMin:
+      values?.intMaxOverTimeForHolidayInMin * 60 || 0,
+    intMaxOverTimeForOffdayInMin:
+      values?.intMaxOverTimeForOffdayInMin * 60 || 0,
+
     intCalenderId: 0,
     intOTHourShouldBeAboveInMin: values?.intOTHourShouldBeAboveInMin * 60 || 0,
     // numOTRateForBasedOnSalaryRange: values?.otRatePerMin || 0,
-    numOTRateForBasedOnSalaryRange: +((values?.otRatePerMin || 0) / 60).toFixed(6), // convert hours to min (user input as hours but we need to save as min)
+    numOTRateForBasedOnSalaryRange: +((values?.otRatePerMin || 0) / 60).toFixed(
+      6
+    ), // convert hours to min (user input as hours but we need to save as min)
+    rangeBasedVM: serviceLengthList || [],
   };
   const payload: any = generateRows(
     policy, // policyType
@@ -295,7 +334,7 @@ const getMatchingPolicy = (values: any, allData: any) => {
 };
 
 // OT Policy Initial Data Generate by GetOverTimeConfigById
-export const initDataGenerate = (data: any) => {
+export const initDataGenerate = (data: any, setTableData: any) => {
   const policyTypeInfo: any = [];
 
   if (data?.intHrPositionId) {
@@ -322,6 +361,19 @@ export const initDataGenerate = (data: any) => {
       label: "Calendar Name",
     });
   }
+  setTableData(
+    data?.rangeBasedVM?.map((itm: any) => {
+      return {
+        ...itm,
+        fromMin: itm?.intFromMinute,
+        toMin: itm?.intToMinute,
+        overTimeAmount: itm?.intOvertimeMinute,
+        showInDepend: {
+          label: itm?.isAtActual ? "Actual" : "Fixed",
+        },
+      };
+    })
+  );
   const formData = {
     policyType: policyTypeInfo,
     policyName: data?.strPolicyName,
@@ -341,12 +393,15 @@ export const initDataGenerate = (data: any) => {
         label: data?.employmentType,
       },
     ],
-    calendarName: data?.intCalenderId ? [
-      {
-        value: data?.intCalenderId,
-        label: data?.strCalenderName,
-      },
-    ] : undefined,
+
+    calendarName: data?.intCalenderId
+      ? [
+          {
+            value: data?.intCalenderId,
+            label: data?.strCalenderName,
+          },
+        ]
+      : undefined,
     fromSalary: data?.numFromSalary,
     toSalary: data?.numToSalary,
     overtimeDependsOn: otDependsOn?.find(
@@ -366,11 +421,13 @@ export const initDataGenerate = (data: any) => {
     maxOverTimeDaily: data?.intMaxOverTimeDaily,
     maxOverTimeMonthly: data?.intMaxOverTimeMonthly,
     overtimeCount: data?.intOtcalculationShouldBe,
-    overtimeAmount: OTCountFrom?.find(
+    overtimeAmount: OTCountAmount?.find(
       (ot) => ot.value === data?.intOtAmountShouldBe
     )?.value,
     calculateAutoAttendance: data?.isOvertimeAutoCalculate,
-    intOTHourShouldBeAboveInMin: data?.intOTHourShouldBeAboveInMin ? (data?.intOTHourShouldBeAboveInMin / 60)?.toFixed(2) : 0,
+    intOTHourShouldBeAboveInMin: data?.intOTHourShouldBeAboveInMin
+      ? (data?.intOTHourShouldBeAboveInMin / 60)?.toFixed(2)
+      : 0,
     intOtconfigId: data?.intOtconfigId,
     isOvertimeAutoCalculate: data?.isOvertimeAutoCalculate,
     isHolidayCountAsFullDayOt: data?.isHolidayCountAsFullDayOt,
@@ -382,9 +439,12 @@ export const initDataGenerate = (data: any) => {
         ? true
         : 2,
     otRatePerMin: ((data?.numOTRateForBasedOnSalaryRange ?? 0) * 60).toFixed(2),
-    intMaxOverTimeForHolidayInMin: data?.intMaxOverTimeForHolidayInMin ? (data?.intMaxOverTimeForHolidayInMin / 60)?.toFixed(2) : 0,
-    intMaxOverTimeForOffdayInMin: data?.intMaxOverTimeForOffdayInMin ? (data?.intMaxOverTimeForOffdayInMin / 60).toFixed(2) : 0,
-
+    intMaxOverTimeForHolidayInMin: data?.intMaxOverTimeForHolidayInMin
+      ? (data?.intMaxOverTimeForHolidayInMin / 60)?.toFixed(2)
+      : 0,
+    intMaxOverTimeForOffdayInMin: data?.intMaxOverTimeForOffdayInMin
+      ? (data?.intMaxOverTimeForOffdayInMin / 60).toFixed(2)
+      : 0,
   };
   return formData;
 };
