171ad013fa31371f564e0797568773ab749a8144
Khurshida Meem <meemrumi59@gmail.com>
2024-11-13 23:07:15 +0600

    dnd-kit setup

--- src/modules/exit-interview/question-creation/add-edit/index.tsx
+++ src/modules/exit-interview/question-creation/add-edit/index.tsx
@@ -1,19 +1,34 @@
+/*
+ * Title: Exit interview add and edit
+ * Author: Khurshida Meem
+ * Date: 12-11-2024
+ *
+ */
+
 import { Col, Form, Row } from "antd";
 import NotPermittedPage from "common/notPermitted/NotPermittedPage";
 import { setFirstLevelNameAction } from "commonRedux/reduxForLocalStorage/actions";
-import {
-  PCard,
-  PCardBody,
-  PCardHeader,
-  PForm,
-  PInput,
-  PSelect,
-} from "Components";
+import { Flex, PCard, PCardHeader, PForm, PInput, PSelect } from "Components";
 import React, { useEffect, useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { useApiRequest } from "../../../../Hooks";
 import { getWorkplaceDDL, getWorkplaceGroupDDL } from "common/api/commonApi";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useApiRequest } from "Hooks";
+import { SortableList } from "../components";
 
 const QuestionCreationAddEdit = () => {
   // Router state
@@ -44,6 +59,7 @@ const QuestionCreationAddEdit = () => {
 
   //   states
   const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([{ id: 1 }, { id: 2 }, { id: 3 }]);
 
   // menu permission
   let letterConfigPermission: any = null;
@@ -80,109 +96,119 @@ const QuestionCreationAddEdit = () => {
             },
           ]}
         />
-        <PCardBody>
-          <Row gutter={[10, 2]}>
-            <Col md={6} sm={24}>
-              <PSelect
-                options={
-                  addLabelValue(
-                    businessUnitDDL,
-                    "BusinessUnitName",
-                    "BusinessUnitId"
-                  ) || []
-                }
-                name="buDDL"
-                label="Business Unit"
-                placeholder="Business Unit"
-                onChange={(value: number, op) => {
-                  form.setFieldsValue({
-                    buDDL: op,
-                    wgDDL: null,
-                    wDDL: null,
-                  });
-                  getWorkplaceGroupDDL({
-                    workplaceGroupDDL,
-                    orgId,
-                    buId: value,
-                  });
-                }}
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
+        <Row gutter={[10, 2]}>
+          <Col md={6} sm={24}>
+            <PSelect
+              options={
+                addLabelValue(
+                  businessUnitDDL,
+                  "BusinessUnitName",
+                  "BusinessUnitId"
+                ) || []
+              }
+              name="buDDL"
+              label="Business Unit"
+              placeholder="Business Unit"
+              onChange={(value: number, op) => {
+                form.setFieldsValue({
+                  buDDL: op,
+                  wgDDL: null,
+                  wDDL: null,
+                });
+                getWorkplaceGroupDDL({
+                  workplaceGroupDDL,
+                  orgId,
+                  buId: value,
+                });
+              }}
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
 
-            <Col md={6} sm={24}>
-              <PSelect
-                options={workplaceGroupDDL?.data || []}
-                name="wgDDL"
-                label="Workplace Group"
-                placeholder="Workplace Group"
-                onChange={(value, op) => {
-                  const { buId } = form.getFieldsValue(true);
-                  form.setFieldsValue({
-                    wgDDL: op,
-                    wDDL: null,
-                  });
-                  getWorkplaceDDL({
-                    workplaceDDL,
-                    orgId,
-                    buId: buId?.value,
-                    wgId: value,
-                  });
-                }}
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
-            <Col md={6} sm={24}>
-              <PSelect
-                options={workplaceDDL?.data || []}
-                name="wDDL"
-                label="Workplace"
-                placeholder="Workplace"
-                onChange={(value, op) => {
-                  form.setFieldsValue({
-                    wDDL: op,
-                  });
-                }}
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
-          </Row>
-          <Row gutter={[10, 2]}>
-            <Col md={6} sm={24}>
-              <PSelect
-                options={[]}
-                name="survayType"
-                label="Survay Type"
-                placeholder="Survay Type"
-                onChange={(_: number, op) => {
-                  form.setFieldsValue({
-                    survayType: op,
-                  });
-                }}
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
+          <Col md={6} sm={24}>
+            <PSelect
+              options={workplaceGroupDDL?.data || []}
+              name="wgDDL"
+              label="Workplace Group"
+              placeholder="Workplace Group"
+              onChange={(value, op) => {
+                const { buId } = form.getFieldsValue(true);
+                form.setFieldsValue({
+                  wgDDL: op,
+                  wDDL: null,
+                });
+                getWorkplaceDDL({
+                  workplaceDDL,
+                  orgId,
+                  buId: buId?.value,
+                  wgId: value,
+                });
+              }}
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
+          <Col md={6} sm={24}>
+            <PSelect
+              options={workplaceDDL?.data || []}
+              name="wDDL"
+              label="Workplace"
+              placeholder="Workplace"
+              onChange={(value, op) => {
+                form.setFieldsValue({
+                  wDDL: op,
+                });
+              }}
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
+        </Row>
+        <Row gutter={[10, 2]}>
+          <Col md={6} sm={24}>
+            <PSelect
+              options={[]}
+              name="survayType"
+              label="Survay Type"
+              placeholder="Survay Type"
+              onChange={(_: number, op) => {
+                form.setFieldsValue({
+                  survayType: op,
+                });
+              }}
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
 
-            <Col md={6} sm={24}>
-              <PInput
-                type="text"
-                name="survayTitle"
-                placeholder="Survay Title"
-                label="Survay Title"
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
-            <Col md={6} sm={24}>
-              <PInput
-                type="text"
-                name="survayDescription"
-                placeholder="Survay Description"
-                label="Survay Description"
-                rules={[{ required: true, message: "Required Field" }]}
-              />
-            </Col>
-          </Row>
-        </PCardBody>
+          <Col md={6} sm={24}>
+            <PInput
+              type="text"
+              name="survayTitle"
+              placeholder="Survay Title"
+              label="Survay Title"
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
+          <Col md={6} sm={24}>
+            <PInput
+              type="text"
+              name="survayDescription"
+              placeholder="Survay Description"
+              label="Survay Description"
+              rules={[{ required: true, message: "Required Field" }]}
+            />
+          </Col>
+        </Row>
+        <div>
+          <SortableList
+            items={items}
+            onChange={setItems}
+            renderItem={(item: any) => (
+              <SortableList.Item id={item.id}>
+                hi
+                <SortableList.DragHandle />
+              </SortableList.Item>
+            )}
+          />
+        </div>
       </PCard>
     </PForm>
   ) : (
