92add807ca442a5458f2e68a753c37bb6d2c0c70
adnan <adnan@ibos.io>
2024-12-15 15:34:58 +0600

    inventory landing

--- src/modules/TrainingAndDevelopment/reports/trainingInventory/inventory.tsx
+++ src/modules/TrainingAndDevelopment/reports/trainingInventory/inventory.tsx
@@ -13,9 +13,10 @@ import {
   PCardHeader,
   PForm,
   PInput,
+  PSelect,
 } from "Components";
 import { getSerial } from "Utils";
-import { Col, Form, Row, Tooltip } from "antd";
+import { Col, Form, Row, Space, Tooltip } from "antd";
 import Loading from "common/loading/Loading";
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
@@ -24,6 +25,8 @@ import useAxiosGet from "utility/customHooks/useAxiosGet";
 import { PModal } from "Components/Modal";
 import Chips from "common/Chips";
 import UserInfoCommonField from "../userInfoCommonField";
+import { getEnumData } from "common/api/commonApi";
+import { filter } from "lodash";
 const TnDInventory = () => {
   // router states
   const history = useHistory();
@@ -33,144 +36,125 @@ const TnDInventory = () => {
 
   // state
   const [loading, setLoading] = useState(false);
-  const [viewModal, setViewModalModal] = useState(false);
-  const [viewData, setViewData] = useState<any>(null);
+  const [
+    trainingTypeDDL,
+    getTrainingTypeDDL,
+    loadingTrainingType,
+    setTrainingType,
+  ] = useAxiosGet();
+
+  const [
+    trainingTitleDDL,
+    getTrainingTitleDDL,
+    loadingTrainingTitle,
+    setTrainingTitle,
+  ] = useAxiosGet();
+
+  const [trainingModeStatusDDL, setTrainingModeStatusDDL] = useState<any>([]);
 
   // Form Instance
   const [form] = Form.useForm();
   // table column
-  const header: any = [
+  const header = [
     {
       title: "SL",
-      render: (_: any, rec: any, index: number) =>
-        getSerial({
-          // currentPage: landingApi?.data?.currentPage,
-          // pageSize: landingApi?.data?.pageSize,
-          currentPage: 1,
-          pageSize: 2000,
-          index,
-        }),
-      fixed: "left",
-      align: "center",
+      render: (_: any, __: any, index: number) => index + 1,
     },
     {
-      title: "Requestor",
-      dataIndex: "employmentName",
+      title: "Workplace Group",
+      dataIndex: "workplaceGroup",
       filter: true,
-      filterKey: "requestorList",
+      filterKey: "workplaceGroupList",
       filterSearch: true,
     },
     {
-      title: "Training Type",
-      dataIndex: "trainingTypeName",
+      title: "Workplace",
+      dataIndex: "workplace",
       filter: true,
-      filterKey: "trainingTypeList",
+      filterKey: "workplaceList",
       filterSearch: true,
     },
-    // {
-    //   title: "Created by",
-    //   dataIndex: "createdBy",
-    //   filter: true,
-    //   filterKey: "createdByList",
-    //   filterSearch: true,
-    // },
-    // {
-    //   title: "Created Date",
-    //   dataIndex: "createdDate",
-    //   render: (data: any) => dateFormatter(data),
-    //   filter: true,
-    //   sorter: true,
-    // },
     {
-      title: "Status",
-      dataIndex: "status",
+      title: "Employee Name",
+      dataIndex: "employeeName",
+    },
+    {
+      title: "Designation",
+      dataIndex: "designation",
+    },
+    {
+      title: "Department",
+      dataIndex: "department",
+    },
+    {
+      title: "No of Training",
+      dataIndex: "noOfTraining",
       filter: true,
-      filterKey: "statusList",
+      filterKey: "noOfTrainingList",
       filterSearch: true,
-      render: (status: any) => {
-        let statusClass = "secondary p-2 rounded-5"; // Default class
-
-        if (status?.label === "Pending") {
-          statusClass = "success p-2 rounded-5";
-        } else if (status?.label === "Assigned") {
-          statusClass = "secondary p-2 rounded-5";
-        } else if (status?.label === "Deferred") {
-          statusClass = "warning p-2 rounded-5";
-        }
-
-        return (
-          <div>
-            <Chips label={status?.label} classess={statusClass} />
-          </div>
-        );
-      },
-      width: 30,
+    },
+    {
+      title: "Training Hour",
+      dataIndex: "trainingHour",
+    },
+    {
+      title: "Total Training Cost",
+      dataIndex: "totalTrainingCost",
+      filter: true,
+      filterKey: "totalTrainingCostList",
+      filterSearch: true,
     },
     {
       title: "Action",
-      dataIndex: "letterGenerateId",
-      render: (generateId: number, rec: any) => (
-        <Flex justify="center">
-          <Tooltip placement="bottom" title={"View"}>
-            <EyeOutlined
-              style={{ color: "green", fontSize: "14px", cursor: "pointer" }}
-              onClick={() => {
-                //   ViewTrainingRequistion(rec?.id, setLoading, setViewData, () => {
-                //     setViewModalModal(true);
-                //   });
-                // history.push("/trainingAndDevelopment/requisition/view", {
-                //   data: rec,
-                // });
-              }}
-            />
-          </Tooltip>
-          <Tooltip placement="bottom" title={"Edit"}>
-            <EditOutlined
+      dataIndex: "status",
+      render: (_: any, rec: any) => (
+        <Space size="middle">
+          <Tooltip placement="bottom" title="Delete">
+            {/* <DeleteOutlined
               style={{
-                color: "green",
-                fontSize: "14px",
-                cursor: "pointer",
-                margin: "0 5px",
+                color: 'red',
+                fontSize: '14px',
+                cursor: 'pointer',
+                margin: '0 5px',
               }}
               onClick={() => {
-                //   ViewTrainingRequistion(
-                //     rec?.id,
-                //     setLoading,
-                //     setViewData,
-                //     (d: any) => {
-                //       history.push("/trainingAndDevelopment/requisition/edit", {
-                //         data: d,
-                //       });
-                //     }
-                //   );
+                const updatedperticipantField = trainingTime.filter(
+                  (item: any) => item.id !== rec.id
+                );
+                changeTrainingStatus(form, updatedperticipantField);
+                setTrainingTime(updatedperticipantField);
               }}
-            />
+            /> */}
           </Tooltip>
-          {/* <Tooltip placement="bottom" title={"Status"}>
-              <BarsOutlined
-                style={{
-                  color: "green",
-                  fontSize: "14px",
-                  cursor: "pointer",
-                  margin: "0 5px",
-                }}
-                onClick={() => {
-                  history.push("/trainingAndDevelopment/requisition/status", {
-                    data: rec,
-                  });
-                }}
-              />
-            </Tooltip> */}
-        </Flex>
+        </Space>
       ),
       align: "center",
+      width: 40,
     },
   ];
+  const typeDataSetForType = (data: any) => {
+    const list: any[] = [];
+    data?.map((d: any) => {
+      if (d?.isActive === true) list.push({ label: d?.name, value: d?.id });
+    });
+    setTrainingType(list);
+  };
+
+  const typeDataSetForTitle = (data: any) => {
+    const list: any[] = [];
+    data?.map((d: any) => {
+      if (d?.isActive === true) list.push({ label: d?.name, value: d?.id });
+    });
+    setTrainingTitle(list);
+  };
   const landingApiCall = (values: any) => {
     getLandingApi("/TrainingRequisition/Training/TrainingRequisition");
   };
   useEffect(() => {
     landingApiCall({});
+    getTrainingTypeDDL("/TrainingType/Training/Type", typeDataSetForType);
+    getTrainingTitleDDL("/TrainingTitle/Training/Title", typeDataSetForTitle);
+    getEnumData("TrainingModeStatus", setTrainingModeStatusDDL);
   }, []);
 
   return (
@@ -196,6 +180,101 @@ const TnDInventory = () => {
           <PCardBody>
             <Row gutter={[10, 2]}>
               <UserInfoCommonField form={form} />
+              <Col md={6} sm={12} xs={24}>
+                <PSelect
+                  options={trainingTypeDDL || []}
+                  name="trainingType"
+                  label={"Training Type"}
+                  placeholder="Training Type"
+                  onChange={(value, op) => {
+                    form.setFieldsValue({
+                      trainingType: op,
+                    });
+                  }}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Training Type is required",
+                    },
+                  ]}
+                />
+              </Col>
+              <Col md={6} sm={12} xs={24}>
+                <PSelect
+                  options={trainingTitleDDL || []}
+                  name="trainingTitle"
+                  label={"Training Title"}
+                  placeholder="Training Title"
+                  onChange={(value, op) => {
+                    form.setFieldsValue({
+                      trainingTitle: op,
+                    });
+                  }}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Training Title is required",
+                    },
+                  ]}
+                />
+              </Col>
+              <Col md={6} sm={12} xs={24}>
+                <PSelect
+                  options={trainingModeStatusDDL || []}
+                  name="trainingMode"
+                  label="Training Mode"
+                  placeholder="Training Mode"
+                  onChange={(value, op) => {
+                    form.setFieldsValue({
+                      trainingMode: op,
+                    });
+                  }}
+                  rules={[
+                    {
+                      required: true,
+                      message: "Training Mode is required",
+                    },
+                  ]}
+                />
+              </Col>
+              <Col md={6} sm={24}>
+                <PInput
+                  type="date"
+                  name="fromDate"
+                  label="From Date"
+                  placeholder="From Date"
+                  onChange={(value) => {
+                    form.setFieldsValue({
+                      fromDate: value,
+                    });
+                  }}
+                  rules={[
+                    {
+                      required: true,
+                      message: "From Date is required",
+                    },
+                  ]}
+                />
+              </Col>
+              <Col md={6} sm={24}>
+                <PInput
+                  type="date"
+                  name="toDate"
+                  label="To Date"
+                  placeholder="To Date"
+                  onChange={(value) => {
+                    form.setFieldsValue({
+                      toDate: value,
+                    });
+                  }}
+                  rules={[
+                    {
+                      required: true,
+                      message: "To Date is required",
+                    },
+                  ]}
+                />
+              </Col>
               <Col md={6} sm={24}>
                 <PButton
                   style={{ marginTop: "22px" }}
