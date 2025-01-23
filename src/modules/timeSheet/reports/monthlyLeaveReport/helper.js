8b902737091e96bd23985e39437afa6584fb8899
Tawfique <tawfique@ibos.io>
2025-01-23 12:13:32 +0600

    Total Approver,Approved Application, ,Pending Application  column added on excel

--- src/modules/timeSheet/reports/monthlyLeaveReport/helper.js
+++ src/modules/timeSheet/reports/monthlyLeaveReport/helper.js
@@ -16,4 +16,7 @@ export const column = {
   HalfDayHours: "Half Day Hours",
   TotalDays: "Days",
   ApplicationDate: "Application Date",
+  TotalApprover: "Total Approver",
+  ApprovedApplications: "Approved Application",
+  PendingApplications: "Pending Application"
 };
