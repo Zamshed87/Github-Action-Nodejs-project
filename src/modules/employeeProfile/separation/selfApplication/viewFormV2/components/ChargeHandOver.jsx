389883d621091ae23832f1d09757b4b4a66a0042
Tawfique <tawfique@ibos.io>
2025-02-12 14:57:32 +0600

    Change Hand Over Modal Created

---
+++ src/modules/employeeProfile/separation/selfApplication/viewFormV2/components/ChargeHandOver.jsx
@@ -0,0 +1,13 @@
+import { Card } from "antd";
+
+export default function ChargeHandOver() {
+  return (
+    <div className="d-flex">
+      <Card
+        style={{
+          width: "100%",
+        }}
+      ></Card>
+    </div>
+  );
+}
