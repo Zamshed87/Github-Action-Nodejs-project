613a25d721eebfcd397a61a9437481c1b828c260
alamin <alameen@ibos.io>
2025-03-10 11:47:58 +0600

    udpate: modify on time

--- src/utility/formatTime12Hour.js
+++ src/utility/formatTime12Hour.js
@@ -1,10 +1,13 @@
 export const formatTime12Hour = (time) => {
-    if (!time) return "N/A"; // Handle empty/null values
+  if (!time) return "N/A"; // Handle empty/null values
   
-    const dateObj = new Date(`1970-01-01T${time}Z`); // Parse as UTC time
-    return dateObj.toLocaleString("en-US", {
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    });
-  };
+  const [hours, minutes] = time.split(":");
+  const dateObj = new Date();  // Get the current date
+  dateObj.setHours(hours, minutes, 0, 0);  // Set the time
+
+  return dateObj.toLocaleString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
