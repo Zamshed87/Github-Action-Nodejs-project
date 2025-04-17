f4c660f2f909638c1af6d20ebbb00736b8d9465c
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-04-17 09:51:16 +0600

    :zap: bank salary report download option added

--- src/modules/CompensationBenefits/reports/bankSalaryReport/hooks/useBankSalaryReport.js
+++ src/modules/CompensationBenefits/reports/bankSalaryReport/hooks/useBankSalaryReport.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import useAxiosGet from "utility/customHooks/useAxiosGet";
 import { downloadFile } from "utility/downloadFile";
 
@@ -12,19 +13,35 @@ const useBankSalaryReport = ({ form }) => {
     const { WorkplaceGroupId, Year, Month, ReportTypeId } =
       form?.getFieldsValue(true);
     getReportData(
-      `/Payroll/BankSalaryReport?WorkplaceGroupId=${WorkplaceGroupId}&Year=${Year}&Month=${Month}&ReportTypeId=${ReportTypeId}`,
+      `/PdfAndExcelReport/Payroll/BankSalaryReport?WorkplaceGroupId=${WorkplaceGroupId}&Year=${Year}&Month=${Month}&ReportTypeId=${ReportTypeId}`,
       (res) => {
         setReportData(res);
       }
     );
   };
-  const downloadExcel = () => {
-    const url = `/PdfAndExcelReport/PMS/YearlyPerformanceReportExcel`;
-    downloadFile(url, `YearlyPerformanceReport`, "xlsx", setLoadingExcel);
+  const downloadExcel = async () => {
+    try {
+      const { WorkplaceGroupId, ReportTypeId } = await form.validateFields(); // validate first
+      const { Year, Month } = form?.getFieldsValue(true);
+      
+      const url = `/PdfAndExcelReport/Payroll/BankSalaryReportExcel?WorkplaceGroupId=${WorkplaceGroupId}&Year=${Year}&Month=${Month}&ReportTypeId=${ReportTypeId}`;
+      downloadFile(url, `BankSalaryReport`, "xlsx", setLoadingExcel);
+    } catch (error) {
+      // Validation failed — do nothing or show a toast if needed
+      toast.error("Please Select the required fields.");
+    }
   };
-  const downloadPdf = () => {
-    const url = `/PdfAndExcelReport/PMS/YearlyPerformanceReportExcel`;
-    downloadFile(url, `YearlyPerformanceReport`, "xlsx", setLoadingPdf);
+
+  const downloadPdf = async () => {
+    try {
+      const { WorkplaceGroupId, ReportTypeId } = await form.validateFields(); // validate first
+      const { Year, Month } = form?.getFieldsValue(true);
+
+      const url = `/PdfAndExcelReport/Payroll/BankSalaryReportPdf?WorkplaceGroupId=${WorkplaceGroupId}&Year=${Year}&Month=${Month}&ReportTypeId=${ReportTypeId}`;
+      downloadFile(url, `BankSalaryReport`, "pdf", setLoadingPdf);
+    } catch (error) {
+      toast.error("Please Select the required fields.");
+    }
   };
 
   return {
