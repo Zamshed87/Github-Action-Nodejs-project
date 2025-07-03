6231878b98cd49ec262e6684b6c111fb821a4c2a
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2025-07-03 11:09:12 +0600

    :zap: comment out loan condition

--- src/modules/employeeProfile/pfLoan/PfLoanAddEdit.jsx
+++ src/modules/employeeProfile/pfLoan/PfLoanAddEdit.jsx
@@ -348,11 +348,12 @@ const PfLoanAddEdit = () => {
                         "Loan amount should be less than 70% of Own PF amount"
                       );
                     }
-                    if (orgId === 15 && e.target.value > totalPfAmount * 0.85) {
-                      return toast.warning(
-                        "Loan amount should be less than 85% of total PF amount"
-                      );
-                    }
+                    // off by sabbir bhai 
+                    // if (orgId === 15 && e.target.value > totalPfAmount * 0.85) {
+                    //   return toast.warning(
+                    //     "Loan amount should be less than 85% of total PF amount"
+                    //   );
+                    // }
                     setFieldValue("loanAmount", e.target.value);
                     setGeneratedData([]);
                   }}
